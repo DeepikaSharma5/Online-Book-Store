40724c9bcf8bead0de41219dd91f23bc77bb8cfe
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-08-05 20:40:37 +0530

    Create UI and validation for contact us form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  HashRouter,
+} from "react-router-dom";
+
+import ContactUs from "./Components/Customer/ContactUs/contactUs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      {/**Customer side*/}
+      <Route component={ContactUs} path='/contact-us'></Route>
+    
+      {/**Admin side*/}
+
+    </Router>
   );
 }
 
