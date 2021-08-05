8a3d8fcd639f4066aad975f7b6ad006cce4d85d4
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-08-05 20:41:05 +0530

    Configure backend to send email

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
+const nodemailer = require('nodemailer')
 
 require("dotenv").config()
 const app = express()
 const port = process.env.PORT || 6060
 app.use(cors())
 app.use(express.json())
+
+const password = process.env.EMAIL_PASSWORD;
 
 const url = process.env.ATLAS_URI
 global.URL = url
@@ -20,6 +23,42 @@ connection.once("open", () => {
 
 app.get('/',(req,res) => {
     res.send("Running Successfully")
+})
+
+//Contact Us Email sending configuration
+app.post("/contactdata", (req, res) => {
+  let data = req.body
+  let smtpTransoprt = nodemailer.createTransport({
+    service: "gmail",
+    port: 465,
+    auth: {
+      user: "2021segroup30@gmail.com",
+      pass: password,
+    },
+  })
+  let mailOptions = {
+    from: data.email,
+    to: "2021segroup30@gmail.com",
+    subject: `Message from ${data.name}`,
+    html: `
+        <h3>Informations</h3>
+        <ul>
+            <li>Name: ${data.name}</li>
+            <li>Email: ${data.email}</li>
+            <li>Subject: ${data.subject}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${data.message}</p>
+        `,
+  }
+  smtpTransoprt.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(info)
+    }
+  })
+  smtpTransoprt.close()
 })
 
 app.listen(port, () => {
