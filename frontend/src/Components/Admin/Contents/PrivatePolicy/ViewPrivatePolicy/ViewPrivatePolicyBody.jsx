3856ce45b84a420f28f06371b9e91686611dcf23
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-08-12 18:22:06 +0530

    Create delete function for private policy

--- frontend/src/Components/Admin/Contents/PrivatePolicy/ViewPrivatePolicy/ViewPrivatePolicyBody.jsx
+++ frontend/src/Components/Admin/Contents/PrivatePolicy/ViewPrivatePolicy/ViewPrivatePolicyBody.jsx
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
+import {Trash, Pencil} from 'react-bootstrap-icons';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 class PrivatePolicyBody extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.deleteData = this.deleteData.bind(this);
+        this.state = { 
+            policy:[]
+         }
     }
+
+    componentDidMount() {
+        axios.get('http://localhost:6060/private-policy/view')
+            .then(response => {
+                const policy = response.data;
+                this.setState({ policy });
+                console.log("response", response);
+            }).catch(error => {
+                alert(error.message);
+                console.log("Error", error);
+            });
+    }
+
+    deleteData(id) {
+        axios.delete('http://localhost:6060/private-policy/delete/' + id)
+            .then(() => {
+                Swal.fire({
+                    title: "Are you sure want to delete?",
+                    text: "You won't be able to revert this!",
+                    icon: "warning",
+                    showCancelButton:"true",
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then(okay => {
+                    if (okay.isConfirmed) {
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                    }
+                });
+            }).catch((err) => {
+                Swal.fire({
+                    title: "error!",
+                    text: "About Us Deleting Not Success",
+                    icon: 'error',
+                    position: 'center',
+                    showConfirmButton: false,
+                    timer: 2000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                    }
+                });
+            });
+    }
+
     render() { 
         return ( 
             <div>
@@ -20,22 +76,28 @@ class PrivatePolicyBody extends Component {
                                         <th scope="col">Delete</th>
                                     </tr>
                                 </thead>
-                                {/* {this.state?.dates?.length > 0 && this.state.dates.map((item, index) => */}
-                                    <tbody>
+                                {this.state?.policy?.length > 0 && this.state.policy.map((item, index) =>
+                                    <tbody key={index}>
                                         <tr>
-                                            <td>sample1</td>
-                                            <td>sample2</td>
+                                            <td>{item.heading}</td>
+                                            <td>{item.details}</td>
                                             <td>
+                                                <button type="button" className="btn btn-default">
+                                                    <Pencil/>Update
+                                                </button>
                                                 {/* <button type="button" className="btn btn-warning"
                                                     onClick={() => this.updateAboutus(item._id, item.description, item.dateRange, item.conferenceStart, item.conferenceEnd, item.conferenceWebsite, item.organizerPhone, item.organizerEmail, item.organizerWebsite, item.status)}>Edit</button> */}
                                             </td>
                                             <td>
+                                                <button type="button" className="btn btn-default" onClick={() => this.deleteData(item._id)}>
+                                                    <Trash/>Delete
+                                                </button>
                                                 {/* <button type="button" className="btn btn-danger"
                                                     onClick={() => this.deleteData(item._id)}>Delete</button> */}
                                             </td>
                                         </tr>
                                     </tbody>
-                                {/* )} */}
+                                )}
                             </table>
                         </div>
                     </div>
