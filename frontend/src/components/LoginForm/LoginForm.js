49c0b279c3a699c1b09ddaf1652859b182f044e4
Methmi Nugawela <methmi.nuga@gmail.com>
2021-08-13 23:52:42 +0530

    Updated styles

--- frontend/src/components/LoginForm/LoginForm.js
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -5,26 +5,26 @@ import styles from "./LoginForm.module.scss";
 
 const LoginForm = () => {
   return (
-    <React.Fragment>
-      <form style={{ marginTop: "70px" }}>
-        <Grid alignItems="center" container direction="column">
-          <TextField
-            label="Email"
-            variant="outlined"
-            className={styles.textField}
-          />
-          <TextField
-            label="Password"
-            variant="outlined"
-            type="password"
-            className={styles.textField}
-          />
-          <Box textAlign="center">
-            <Button className={styles.signInBtn}>LOG IN</Button>
-          </Box>
-        </Grid>
-      </form>
-    </React.Fragment>
+      <React.Fragment>
+        <form style={{ marginTop: "70px" }}>
+          <Grid alignItems="center" container direction="column">
+            <TextField
+              label="Email"
+              variant="outlined"
+              className={styles.textField}
+            />
+            <TextField
+              label="Password"
+              variant="outlined"
+              type="password"
+              className={styles.textField}
+            />
+            <Box textAlign="center">
+              <Button className={styles.signInBtn}>LOG IN</Button>
+            </Box>
+          </Grid>
+        </form>
+      </React.Fragment>
   );
 };
 
