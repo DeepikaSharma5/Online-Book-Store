8228030309d45db94ebf716e6b3c875f3ec66679
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-08-14 10:22:56 +0530

    Database integration for AboutUs

---
+++ backend/models/AboutUs.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const AboutUsSchema = new Schema(
+    {
+      mission: {
+        type: String,
+        required: true,
+      },
+      missionInfo: {
+        type: String,
+        required: true,
+      },
+      customerService: {
+        type: String,
+        required: true,
+      },
+      convenience: {
+        type: String,
+        required: true,
+      },
+      story1: {
+        type: String,
+        required: true,
+      },
+      story2: {
+        type: String,
+        required: true,
+      },
+      story3: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: String,
+        required: true,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  )
+  
+  const About_Us_Schema = mongoose.model("About_Us_Schema",AboutUsSchema)
+  module.exports = About_Us_Schema
---
+++ backend/routers/AboutUs.js
@@ -0,0 +1,73 @@
+const router = require("express").Router();
+let AboutUs = require("../models/AboutUs");
+
+router.route("/add").post((req, res) => {
+    const mission = req.body.mission;
+    const missionInfo = req.body.missionInfo;
+    const customerService = req.body.customerService;
+    const convenience = req.body.convenience;
+    const story1 = req.body.story1;
+    const story2 = req.body.story2;
+    const story3 = req.body.story3;
+    const date = new Date().toISOString().slice(0,10);
+  
+    const newAboutUs = new AboutUs({
+        mission,
+        missionInfo,
+        customerService,
+        convenience,
+        story1,
+        story2,
+        story3,
+        date
+    })
+  
+    newAboutUs
+      .save()
+      .then(() => res.json("About Us added successfully"))
+      .catch((err) => res.status(400).json("Error : " + err))
+})
+
+
+  router.route("/view").get((req, res) => {
+    AboutUs.find()
+      .then((AboutUs) => res.json(AboutUs))
+      .catch((err) => res.status(400).json("Error: " + err))
+  })
+  
+  router.route("/delete/:id").delete(async (req, res) => {
+    let id = req.params.id
+    await AboutUs.findByIdAndDelete(id)
+      .then(() => {
+        res.status(200).send({ status: "Successfully deleted." })
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(500).send({ status: "Error while deleting.", error: err.message })
+      })
+  })
+
+  router.route("/update/:id").post(async (req, res) => {
+    let id = req.params.id
+    const mission = req.body.mission;
+    const missionInfo = req.body.missionInfo;
+    const customerService = req.body.customerService;
+    const convenience = req.body.convenience;
+    const story1 = req.body.story1;
+    const story2 = req.body.story2;
+    const story3 = req.body.story3;
+    const updateRangeAboutUs = {
+      heading,
+      details,
+    }
+    const updateRange = await AboutUs.findOneAndUpdate({ _id: id }, updateRangeAboutUs)
+      .then(() => {
+        res.status(200).send({ status: "Successfully updated" })
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(500).send({ status: "Error while updating.", error: err.message })
+      })
+  })
+  
+  module.exports = router
