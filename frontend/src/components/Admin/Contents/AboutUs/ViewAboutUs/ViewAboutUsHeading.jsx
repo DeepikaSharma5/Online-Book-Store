50bd721616182550d45a19a13f8044e490ef2235
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-08-18 20:43:04 +0530

    Add nav bar to about us admin side

--- frontend/src/components/Admin/Contents/AboutUs/ViewAboutUs/ViewAboutUsHeading.jsx
+++ frontend/src/components/Admin/Contents/AboutUs/ViewAboutUs/ViewAboutUsHeading.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class ViewAboutUsHeading extends Component {
     render() { 
         return ( 
-            <div className="d-flex p-2" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px'}}>
-                <h4 tag='div' className='display-1 pb-3 mb-3 border-bottom' style={{fontWeight:"bold"}}>ABOUT US</h4>
+            <div className="d-flex p-2" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '250px', position:'relative', left:'50px', marginBottom:'-20px'}}>
+                <h4 tag='div' className='display-1 pb-3 mb-3 border-bottom' style={{fontWeight:"bold", fontSize:'250%'}}>ABOUT US</h4>
             </div>
         );
     }
