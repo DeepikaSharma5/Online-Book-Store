a5883b3edc9a13ef7a7e2c4864e6f3bf86642092
Methmi Nugawela <methmi.nuga@gmail.com>
2021-08-18 18:17:48 +0530

    Base for verification middleware

--- backend/routers/verifyToken.js
+++ backend/routers/verifyToken.js
@@ -7,7 +7,11 @@ module.exports = function (req, res, next) {
     if(!token) return res.status(401).send('Access Denied');
 
     try {
+        // Only authorizes users with a seller token
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET_ADMIN);
 
+        //req.user available through out the app
+        req.user = verified;
         next();
     } catch (error) {
         res.status(400).send('Invalid Token');
