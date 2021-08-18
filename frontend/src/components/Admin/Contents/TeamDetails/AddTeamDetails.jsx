6e458d1f47102e04ccaf6cb784864d573002290b
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-08-18 21:49:14 +0530

    Create team details page in admin side

--- frontend/src/components/Admin/Contents/TeamDetails/AddTeamDetails.jsx
+++ frontend/src/components/Admin/Contents/TeamDetails/AddTeamDetails.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import { APP_ROUTES } from '../../../../utilities/constants/routes.constants';
+import { Folder, Backspace } from 'react-bootstrap-icons';
+import NavBar from '../../NavBar/NavBar';
 
 export default function AddTeamDetails() {
     const [name, setName] = useState("");
@@ -13,7 +15,7 @@ export default function AddTeamDetails() {
         formData.append("file", imageSelected);
         formData.append("upload_preset", "ml_default");
 
-        axios.post("https://api.cloudinary.com/v1_1/spm2021/image/upload", formData).then((response) => {
+        axios.post("https://api.cloudinary.com/v1_1/applicationframework2021/image/upload", formData).then((response) => {
             const image = imageSelected.name;
             const AddTeamDetails = {
                 name,
@@ -24,7 +26,7 @@ export default function AddTeamDetails() {
                 .then(() => {
                     Swal.fire({
                         title: "Success!",
-                        text: "Saved success",
+                        text: "Added Successed!",
                         icon: 'success',
                         confirmButtonText: "OK",
                         type: "success"
@@ -33,38 +35,57 @@ export default function AddTeamDetails() {
                             window.location.href = APP_ROUTES.ADMIN_VIEW_TEAM_DETAILS;
                         }
                     });
+
                 }).catch((err) => {
                     Swal.fire({
-                        title: "Error!",
-                        text: "Unable to save",
+                        title: "error!",
+                        text: "Not Success",
                         icon: 'error',
-                        confirmButtonText: "OK",
-                        type: "success"
-                    })
+                        position: 'center',
+                        showConfirmButton: false,
+                        timer: 2000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.addEventListener('mouseenter', Swal.stopTimer)
+                            toast.addEventListener('mouseleave', Swal.resumeTimer)
+                        }
+                    });
                 })
         })
     }
 
+    function cancel() {
+        window.location.href = APP_ROUTES.ADMIN_VIEW_TEAM_DETAILS;
+    }
+
     return (
         <div>
-            <div className="d-flex p-2" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
-                <h1 tag='div' className='display-1 pb-3 mb-3 border-bottom'>Add TeamDetails</h1>
+            <NavBar />
+            <div className="d-flex p-2" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '250px', position: 'relative', left: '60px' }}>
+                <h1 tag='div' className='display-1 pb-3 mb-3 border-bottom' style={{ fontWeight: "bold", fontSize: '250%' }}>Add TeamDetails</h1>
             </div>
-            <div className="col-md-14 col-sm-12" style={{ maxWidth: '70rem', margin: 'auto', padding: '10px' }}>
-                <div className="row g-0">
-                    <div className="form-outline mb-4">
-                        Name: <input type="text" id="name" className="form-control border border-dark mb-3" name="name" onChange={(e) => { setName(e.target.value) }} required />
-                    </div>
-                    <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="customFile">Add Image</label>
-                        <input type="file" onChange={(e) => { setimageSelected(e.target.files[0]) }} className="form-control" id="customFile" required />
+            <div style={{ height: '500px', position: 'relative', left: '80px' }}>
+                <div className="col-md-14 col-sm-12" style={{ maxWidth: '80rem', margin: 'auto', padding: '30px', borderColor: 'black', background: '#ffffff', boxShadow: '10px 10px 45px #919191,-10px -10px 45px #ffffff' }}>
+                    <div className="col" style={{ borderRadius: '33px', height: '450px' }}>
+                        <div className="row g-0">
+                            <div className="form-outline mb-4">
+                                Name: <label style={{ color: 'red' }}>*</label><input type="text" id="name" className="form-control border border-dark mb-3" name="name" onChange={(e) => { setName(e.target.value) }} required />
+                            </div>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="customFile">Add Image <label style={{ color: 'red' }}>*</label></label>
+                                <input type="file" onChange={(e) => { setimageSelected(e.target.files[0]) }} className="form-control" id="customFile" required />
 
-                    </div>
-                    <div className="form-outline mb-4">
-                        position: <input type="text" id="position" className="form-control border border-dark mb-3" name="position" onChange={(e) => { setPosition(e.target.value) }} required />
+                            </div>
+                            <div className="form-outline mb-4">
+                                position: <label style={{ color: 'red' }}>*</label><input type="text" id="position" className="form-control border border-dark mb-3" name="position" onChange={(e) => { setPosition(e.target.value) }} required />
 
+                            </div>
+                            <div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+                                <button type="button" className="btn btn-outline-danger" style={{ float: 'right', padding: '12px 68px', marginBottom: '30px', fontWeight: 'bold', fontSize: "130%", position: 'relative', left: '200px' }} onClick={cancel}><Backspace /> Cancel</button>
+                                <button type="submit" className="btn btn-outline-success" style={{ float: 'left', padding: '12px 68px', marginBottom: '30px', fontWeight: 'bold', fontSize: "130%", position: 'relative', right: '200px' }} onClick={onSubmit}><Folder /> Save</button>
+                            </div>
+                        </div>
                     </div>
-                    <button type="submit" onClick={onSubmit} className="btn btn-primary btn-block mb-4">Save</button>
                 </div>
             </div>
         </div>
--- frontend/src/components/Admin/Contents/TeamDetails/ViewTeamDetails.jsx
+++ frontend/src/components/Admin/Contents/TeamDetails/ViewTeamDetails.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { PlusLg } from 'react-bootstrap-icons';
+import { Trash, Pencil, PlusLg } from 'react-bootstrap-icons';
 import Swal from 'sweetalert2';
 import { reactLocalStorage } from 'reactjs-localstorage';
 import { APP_ROUTES } from '../../../../utilities/constants/routes.constants';
+import NavBar from '../../NavBar/NavBar';
 
 export default function ViewTeamDetails() {
 
@@ -32,7 +33,7 @@ export default function ViewTeamDetails() {
             }).catch((err) => {
                 Swal.fire({
                     title: "error!",
-                    text: "Technical Committee Member Deleting Not Success",
+                    text: "Deleting Not Success",
                     icon: 'error',
                     position: 'center',
                     showConfirmButton: false,
@@ -48,42 +49,46 @@ export default function ViewTeamDetails() {
 
     function updateTeamDetails(id, name, image, position) {
         reactLocalStorage.setObject("TeamDetails", [id, name, image, position]);
-        window.location.href = "/updatetech";
+        window.location.href = APP_ROUTES.ADMIN_UPDATE_TEAM_DETAILS;
     }
     return (
         <div>
             <div>
-                <div className="d-flex p-2" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
-                    <h1 tag='div' className='display-1 pb-3 mb-3 border-bottom'>VIEW TEAM DETAILS</h1>
+                <NavBar />
+            </div>
+            <div>
+                <div className="d-flex p-2" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '250px', position: 'relative', left: '60px' }}>
+                    <h1 tag='div' className='display-1 pb-3 mb-3 border-bottom' style={{ fontWeight: "bold", fontSize: '250%' }}>VIEW TEAM DETAILS</h1>
                 </div>
             </div>
-            {/* <Navigation/> */}
-            <div className="container">
-                <div>
-                    <button type="button" className="btn btn-info" style={{ float: 'right', padding: '12px 28px', marginBottom: '30px' }} onClick={() => { window.location.href = APP_ROUTES.ADMIN_ADD_TEAM_DETAILS }}>
-                        <PlusLg /> Add new Team Member
-                    </button>
-                </div>
-                <div className="row">
-                    {TeamDetails.map((TeamDetails, key) => (
-                        <div class="col-sm-3 mt-3">
-                            <div class="card">
-                                <div className="text-center">
-                                    <img className="card-img-top " style={{ width: '100%' }} src={'https://api.cloudinary.com/v1_1/spm2021/image/upload//v1629040165/' + TeamDetails.image} alt="Card image cap" />
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title text-center text-capitalize">{TeamDetails.name}</h5>
-                                    <p className="card-text">
-                                        <span className="fw-bold">position : </span>{TeamDetails.position}<br />
-                                    </p>
-                                </div>
-                                <div className="card-footer bg-white border-0 text-end">
-                                    <button type="button" onClick={() => deleteTeamDetails(TeamDetails._id)} className="btn btn-danger btn-sm">Delete <i class="bi bi-trash"></i></button>{' '}
-                                    <button type="button" onClick={() => updateTeamDetails(TeamDetails._id, TeamDetails.name, TeamDetails.image, TeamDetails.position)} class="btn btn-success btn-sm">Update <i class="bi bi-pencil-square"></i></button>
+            <div style={{ position: 'relative', left: '110px' }}>
+                <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '90rem', margin: 'auto', padding: '10px' }}>
+                    <div>
+                        <button type="button" className="btn btn-info" style={{ float: 'right', padding: '12px 28px', marginBottom: '30px' }} onClick={() => { window.location.href = APP_ROUTES.ADMIN_ADD_TEAM_DETAILS }}>
+                            <PlusLg /> Add new Team Member
+                        </button>
+                    </div>
+                    <div className="row">
+                        {TeamDetails.map((TeamDetails, key) => (
+                            <div class="col-sm-3 mt-5">
+                                <div class="card">
+                                    <div className="text-center">
+                                        <img className="card-img-top " style={{ width: '100%' }} src={'https://res.cloudinary.com/applicationframework2021/image/upload/v1624901540/' + TeamDetails.image} alt="Card image cap" />
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className="card-title text-center text-capitalize">{TeamDetails.name}</h5>
+                                        <p className="card-text">
+                                            <span className="fw-bold">position : </span>{TeamDetails.position}<br />
+                                        </p>
+                                    </div>
+                                    <div className="card-footer bg-white border-0 text-end">
+                                        <button type="button" onClick={() => deleteTeamDetails(TeamDetails._id)} className="btn btn-outline-danger"> <Trash /> Delete</button>{' '}
+                                        <button type="button" onClick={() => updateTeamDetails(TeamDetails._id, TeamDetails.name, TeamDetails.image, TeamDetails.position)} class="btn btn-outline-success"><Pencil /> Update </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
