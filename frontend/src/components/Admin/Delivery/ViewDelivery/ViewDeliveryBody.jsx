4c65c2bb4f0ddbede07639bc5efc4647c64bce82
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-08-18 23:07:16 +0530

    Add search and dropdown

--- frontend/src/components/Admin/Delivery/ViewDelivery/ViewDeliveryBody.jsx
+++ frontend/src/components/Admin/Delivery/ViewDelivery/ViewDeliveryBody.jsx
@@ -73,64 +73,78 @@ class PrivatePolicyBody extends Component {
         return (
             <div style={{ position: 'relative', left: '110px' }}>
                 <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '100rem', margin: 'auto', padding: '10px' }}>
-                    <div>
-                        {/* <input type="submit" className="orm-control mr-sm-2" placeholder="Search" style={{ float: 'right', padding: '12px 28px', marginBottom: '30px' }} >
-                            <Search />
-                        </input> */}
+                    <div className="container" >
+                        <div className="row">
+                            <div className="col-8" style={{}}>
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>Pending</option>
+                                    <option>Processing</option>
+                                    <option>Shipped</option>
+                                    <option>Delivered</option>
+                                </select>
+                            </div>
+                            <div className="col-4">
+                                <form class="form-inline">
+                                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                                </form>
+                            </div>                        
+                        </div>
                     </div>
-                    <div className="card overflow-auto" style={{ maxHeight: '400%', background: '#ffffff', boxShadow: '10px 10px 45px #919191,-10px -10px 45px #ffffff' }}>
-                        <div className="card-body">
-                            <div style={{ height: '540px' }}>
-                                <div className="card-body" >
-                                    <div className="row">
-                                        <table className="table table-bordered" style={{ textAlign: 'center' }}>
-                                            <thead className="thead-light" >
+                </div>
+                <div className="card overflow-auto" style={{ maxHeight: '400%', background: '#ffffff', boxShadow: '10px 10px 45px #919191,-10px -10px 45px #ffffff' }}>
+                    <div className="card-body">
+                        <div style={{ height: '540px' }}>
+                            <div className="card-body" >
+                                <div className="row">
+                                    <table className="table table-bordered" style={{ textAlign: 'center' }}>
+                                        <thead className="thead-light" >
+                                            <tr>
+                                                <th scope="col" className="w-15">Order ID</th>
+                                                <th scope="col" className="w-30">Address 1</th>
+                                                <th scope="col" className="w-30">Address 2</th>
+                                                <th scope="col" className="w-30">Address 3</th>
+                                                <th scope="col" className="w-30">Phone Number</th>
+                                                <th scope="col" className="w-20">Order Delivery Status</th>
+                                                <th scope="col" className="w-15">Update</th>
+                                                <th scope="col" className="w-15">Delete</th>
+                                            </tr>
+                                        </thead>
+                                        {this.state?.policy?.length > 0 && this.state.policy.map((item, index) =>
+                                            <tbody key={index}>
                                                 <tr>
-                                                    <th scope="col" className="w-15">Order ID</th>
-                                                    <th scope="col" className="w-30">Address 1</th>
-                                                    <th scope="col" className="w-30">Address 2</th>
-                                                    <th scope="col" className="w-30">Address 3</th>
-                                                    <th scope="col" className="w-30">Phone Number</th>
-                                                    <th scope="col" className="w-20">Order Delivery Status</th>
-                                                    <th scope="col" className="w-15">Update</th>
-                                                    <th scope="col" className="w-15">Delete</th>
+                                                    <td>{item.heading}</td>
+                                                    <td>{item.details}</td>
+                                                    <td>{item.heading}</td>
+                                                    <td>{item.details}</td>
+                                                    <td>{item.heading}</td>
+                                                    <td>{item.details}</td>
+                                                    <td>
+                                                        <button type="button" className="btn btn-outline-success" onClick={() => this.updatePrivatePolicy(item._id, item.heading, item.details)}>
+                                                            <Pencil /> Update
+                                                        </button>
+                                                    </td>
+                                                    <td>
+                                                        <button type="button" className="btn btn-outline-danger" onClick={() => this.deleteData(item._id)}>
+                                                            <Trash /> Delete
+                                                        </button>
+                                                    </td>
                                                 </tr>
-                                            </thead>
-                                            {this.state?.policy?.length > 0 && this.state.policy.map((item, index) =>
-                                                <tbody key={index}>
-                                                    <tr>
-                                                        <td>{item.heading}</td>
-                                                        <td>{item.details}</td>
-                                                        <td>{item.heading}</td>
-                                                        <td>{item.details}</td>
-                                                        <td>{item.heading}</td>
-                                                        <td>{item.details}</td>
-                                                        <td>
-                                                            <button type="button" className="btn btn-outline-success" onClick={() => this.updatePrivatePolicy(item._id, item.heading, item.details)}>
-                                                                <Pencil /> Update
-                                                            </button>
-                                                        </td>
-                                                        <td>
-                                                            <button type="button" className="btn btn-outline-danger" onClick={() => this.deleteData(item._id)}>
-                                                                <Trash /> Delete
-                                                            </button>
-                                                        </td>
-                                                    </tr>
-                                                </tbody>
-                                            )}
-                                        </table>
-                                    </div>
+                                            </tbody>
+                                        )}
+                                    </table>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <button type="button" className="btn btn-info" style={{ float: 'left', padding: '12px 28px', marginTop: '30px' }} >
-                            <FileEarmarkPdf /> Generate Report
-                        </button>
-                    </div>
+                </div>
+                <div>
+                    <button type="button" className="btn btn-info" style={{ float: 'left', padding: '12px 28px', marginTop: '30px' }} >
+                        <FileEarmarkPdf /> Generate Report
+                    </button>
                 </div>
             </div>
+
         );
     }
 }
