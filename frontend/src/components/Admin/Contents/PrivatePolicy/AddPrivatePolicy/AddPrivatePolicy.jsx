cd30b5ee3fe283a5f70704886442f20b1c29bdcf
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-08-21 00:29:39 +0530

    Add Admin side navigation bar

--- frontend/src/components/Admin/Contents/PrivatePolicy/AddPrivatePolicy/AddPrivatePolicy.jsx
+++ frontend/src/components/Admin/Contents/PrivatePolicy/AddPrivatePolicy/AddPrivatePolicy.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import AddPrivatePolicyHeading from './AddPrivatePolicyHeading';
 import AddPrivatePolicyBody from './AddPrivatePolicyBody';
 import NavBar from '../../../NavBar/NavBar';
+import AppBar from '../../../NavBar/AppBar';
 
 class AddPrivatePolicy extends Component {
     render() {
         return (
             <div>
+                <AppBar />
                 <NavBar />
                 <div>
                     <AddPrivatePolicyHeading />
