d6d08220b2202caede0295fec5fc12699a4ad6c4
Methmi Nugawela <methmi.nuga@gmail.com>
2021-08-21 17:35:26 +0530

    Changed header style

--- frontend/src/components/Customer/Homepage/Header/Header.jsx
+++ frontend/src/components/Customer/Homepage/Header/Header.jsx
@@ -84,129 +84,145 @@ export default function Header() {
   const isMenuOpen = Boolean(anchorEl);
 
   const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const preventDefault = (event) => event.preventDefault();
-  const navigateToHome = (event) =>
-    (window.location.href = APP_ROUTES.USER_HOMEPAGE);
-  const NavigateToContactUs = (event) =>
-    (window.location.href = APP_ROUTES.USER_CONTACT_US);
-  const navigateToMyAccount = (event) => {
-    handleMenuClose();
-    window.location.href = APP_ROUTES.USER_PERSONAL_DETAILS;
+      setAnchorEl(event.currentTarget);
   };
 
   const handleMenuClose = () => {
     setAnchorEl(null);
-  };
+};
 
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={navigateToMyAccount}>My account</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My Delivery Address</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My Deliveries</MenuItem>
-    </Menu>
-  );
+const cart = useSelector((state) => state.cart);
+
+const { cartItems } = cart;
+
+const getCartCount = () => {
+  return cartItems.reduce((qty, item) => qty + Number(item.qty), 0);
+};
+
+
+  const navigateToAddCardDetails = (event) => window.location.href = APP_ROUTES.USER_ADD_CARD_DETAILS;
+  const navigateToCardDetails = (event) => window.location.href = APP_ROUTES.USER_CARD_DETAILS;
+  const navigateToCart = (event) => window.location.href = APP_ROUTES.CART;
+
+
+const preventDefault = (event) => event.preventDefault();
+const navigateToHome = (event) => window.location.href = APP_ROUTES.USER_HOMEPAGE;
+const NavigateToBooks = (event) => window.location.href = APP_ROUTES.BOOKS;
+const NavigateToAboutUs = (event) => window.location.href = APP_ROUTES.USER_ABOUT_US;
+const NavigateToContactUs = (event) => window.location.href = APP_ROUTES.USER_CONTACT_US;
+const navigateToSearchResults = (event) => window.location.href = APP_ROUTES.USER_SEARCH_BOOKS;
+const navigateToMyDeliveryAddress = (event) => window.location.href = APP_ROUTES.USER_VIEW_ADDRESS;
+const navigateToMyDeliveries = (event) => window.location.href = APP_ROUTES.USER_MY_DELIVERIES;
+const navigateToMyAccount = (event) => window.location.href = APP_ROUTES.USER_PERSONAL_DETAILS;
+const navigateToMyWishlist = (event) => window.location.href = APP_ROUTES.USER_WISHLIST;
+const navigateToSearchForWishlist = (event) => window.location.href = APP_ROUTES.WISHLIST_SEARCH;
+
+const menuId = 'primary-search-account-menu';
+const renderMenu = (
+  <Menu
+    anchorEl={anchorEl}
+    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+    id={menuId}
+    keepMounted
+    transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+    open={isMenuOpen}
+    onClose={handleMenuClose}
+  >
+    <MenuItem onClick={navigateToMyAccount}> 
+      <User/>&ensp;My account
+    </MenuItem>
+    <MenuItem onClick={navigateToMyWishlist}>
+      <Heart/>&ensp;My Wishlist
+    </MenuItem>
+    <MenuItem onClick={navigateToSearchForWishlist}>
+      <Search/>&ensp;Search for wishlist
+    </MenuItem>
+    <MenuItem onClick={navigateToMyDeliveryAddress}>
+      <Clipboard/>&ensp;My Delivery Address
+    </MenuItem>
+    <MenuItem onClick={navigateToMyDeliveries}>
+      <Truck/>&ensp;My Deliveries
+    </MenuItem>
+
+    <MenuItem onClick={navigateToAddCardDetails}>
+      <PlusSquare/>&ensp;Add Card Details</MenuItem>
+    <MenuItem onClick={navigateToCardDetails}>
+      <CreditCard/>&ensp;Card Details</MenuItem>
+
+  </Menu>
+);
 
   return (
-    <div className={classes.grow}>
-      <AppBar position="static">
-        <Toolbar>
-          {/*Booklab logo*/}
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <LibraryBooksIcon onClick={navigateToHome} />
-          </IconButton>
-          {/*Booklab title*/}
-          <Typography className={classes.title} variant="h5" noWrap>
-            BookLab
-          </Typography>
-          {/*Search Bar*/}
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ "aria-label": "search" }}
-            />
-          </div>
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            <ButtonGroup variant="text" aria-label="text primary button group">
-              <Button onClick={preventDefault}>
-                <Typography
-                  className={classes.title}
-                  style={{ color: "white" }}
-                  variant="h6"
-                  noWrap
-                >
-                  Books
-                </Typography>
-              </Button>
-              <Button onClick={preventDefault}>
-                <Typography
-                  className={classes.title}
-                  style={{ color: "white" }}
-                  variant="h6"
-                  noWrap
-                >
-                  About Us
-                </Typography>
-              </Button>
-              <Button onClick={NavigateToContactUs}>
-                <Typography
-                  className={classes.title}
-                  style={{ color: "white" }}
-                  variant="h6"
-                  noWrap
-                >
-                  Contact Us
-                </Typography>
-              </Button>
-            </ButtonGroup>
-            <IconButton aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <ShoppingCartIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-              <Typography className={classes.title} variant="h5">
-                {/*Add user name here*/}
-                user name
-              </Typography>
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-      {renderMenu}
-    </div>
+      <div className={classes.grow}>
+          <AppBar position="fixed" width='device-width'>
+              <Toolbar>
+                  {/*Booklab logo*/}
+                  <IconButton
+                      edge="start"
+                      className={classes.menuButton}
+                      color="inherit"
+                      aria-label="open drawer"
+                  >
+                      <LibraryBooksIcon onClick={navigateToHome} />
+                  </IconButton>
+                  {/*Booklab title*/}
+                  <Typography className={classes.title} variant="h5" noWrap>
+                      BookLab
+                  </Typography>
+                  {/*Search Bar*/}
+                  <div className={classes.search} style={{"width":"300px", "height":"40px"}}>
+                      <div className={classes.searchIcon}>
+                          <SearchIcon />
+                      </div>
+                      <InputBase onClick={navigateToSearchResults}
+                          placeholder="Search…"
+                          classes={{
+                              root: classes.inputRoot,
+                              input: classes.inputInput,
+                          }}
+                          inputProps={{'aria-label': 'search'}}
+                      />
+                  </div>
+                  <div className={classes.grow} />
+                  <div className={classes.sectionDesktop}>
+                      <ButtonGroup variant="text" aria-label="text primary button group">
+                          <Button onClick={NavigateToBooks}>
+                              <Typography className={classes.title} style={{ color: 'white' }} variant="h6" noWrap>
+                                  Books
+                              </Typography>
+                          </Button>
+                          <Button onClick={NavigateToAboutUs}>
+                              <Typography className={classes.title} style={{ color: 'white'}} variant="h6" noWrap>
+                                  About Us
+                              </Typography></Button>
+                          <Button onClick={NavigateToContactUs} >
+                              <Typography className={classes.title} style={{ color: 'white' }} variant="h6" noWrap>
+                                  Contact Us
+                              </Typography></Button>
+                      </ButtonGroup>
+                      <IconButton aria-label="show 4 new mails" color="inherit">
+                          <Badge badgeContent={getCartCount()} color="secondary">
+                              <ShoppingCartIcon onClick={navigateToCart} />
+                          </Badge>
+                      </IconButton>
+                      <IconButton
+                          edge="end"
+                          aria-label="account of current user"
+                          aria-controls={menuId}
+                          aria-haspopup="true"
+                          onClick={handleProfileMenuOpen}
+                          color="inherit"
+                      >
+                          <AccountCircle />
+                          <Typography className={classes.title} variant="h5" >
+                              {/*Add user name here*/}
+                              username
+                      </Typography>
+                      </IconButton>
+                  </div>
+              </Toolbar>
+          </AppBar>
+          {renderMenu}
+      </div>
   );
 }
