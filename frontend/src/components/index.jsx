c2e7e640749c222a7e32f5f860a2e31dace31061
Methmi Nugawela <methmi.nuga@gmail.com>
2021-09-17 14:11:16 +0530

    implemented logic for super admin to register new admins

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,4 +1,5 @@
 export {default as LoginForm} from "./Login/LoginForm";
+export {default as AdminLoginForm} from "./Login/AdminLoginForm";
 export {default as SignupForm} from "./Signup/SignupForm";
 export {default as AdminTable} from "./SuperAdmin/AdminTable";
 export {default as AdminStatusModal} from "./SuperAdmin/AdminStatusModal";
